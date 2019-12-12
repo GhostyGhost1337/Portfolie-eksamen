3b66d7d2e0682ccf3d11f2fc893b7dbf07f190a5
GhostyGhost1337 <55685673+GhostyGhost1337@users.noreply.github.com>
2019-12-12 08:19:44 +0100

    Kode

---
+++ gestalt.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="da">
+
+	<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Portfolie Tobias H Madsen</title>
+        <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans&display=swap" rel="stylesheet">
+	</head>
+
+        <body>
+            <section id="menu">
+                <?php include 'menu.inc';?>
+                </section>
+            <p>Hello world</p>
+            
+        </body>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="da">
+
+	<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Portfolie Tobias H Madsen</title>
+        <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans&display=swap" rel="stylesheet">
+	</head>
+
+        <body>
+            <section id="htmlcssgrid"> <!--Her starter griddet-->
+                <section id="menu">
+                <?php include 'menu.inc';?>
+                </section>
+                    
+                <section id="overskrift">
+                <h1>HTML og CSS</h1>
+                </section>
+                
+                <section id="htmloverskrift">
+                <h2>HTML</h2>
+                </section>
+                
+                <section id=htmltekst>
+                <p>HTML Tekst</p>
+                </section>
+                
+                <section id="cssoverskrift">
+                <h2>CSS</h2>
+                </section>
+                
+                <section id="csstekst">
+                <p>CSS Tekst</p>
+                </section>
+                
+            </section> <!--Her slutter griddet-->
+        </body>
+      
