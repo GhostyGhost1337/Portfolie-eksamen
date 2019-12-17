8bee77c94bdad5bcd68d91aae29209eccff9407a
GhostyGhost1337 <55685673+GhostyGhost1337@users.noreply.github.com>
2019-12-17 10:50:37 +0100

    Seperate styles og lidt tekst

--- farver.php
+++ farver.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Portfolie Tobias H Madsen</title>
-        <link href="style.css" rel="stylesheet">
+        <link href="farvestyle.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans&display=swap" rel="stylesheet">
 	</head>
 
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Portfolie Tobias H Madsen</title>
-        <link href="style.css" rel="stylesheet">
+        <link href="introstyle.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans&display=swap" rel="stylesheet">
 	</head>
 
@@ -15,6 +15,10 @@
                 <?php include 'menu.inc';?>
                 </section>
                     
+                <section id="intro">
+                <p>Læringsportfolie af Tobias H Madsen 1 semester 2019 multimediedesigner</p>
+                </section>
+                
                 <section id="overskrift">
                 <h1>HTML og CSS</h1>
                 </section>
@@ -24,7 +28,17 @@
                 </section>
                 
                 <section id=htmltekst>
-                <p>HTML Tekst</p>
+                <p>HTML også kendt som HyperText Markup Language er et kodesprog der bruges til kodning af hjemmesider som gøres ved at skrive Tags som beskriver, hvad man laver. 
+                Det kunne være tags som body, hvilket er der man skriver hoveddelen af hjemmesiden i med brug af andre tags såsom p altså paragraf som man oftest sætter sit indhold i p tagget, det skaber også et mellemrum over og under den tekst man skriver i det.
+
+                En vigtig ting i HTML er de semantiske tags som fortæller noget om indholdet f.eks. Hvor vigtigt det er på siden. Det hjælper derfor search engines med deres optimization (seo). 
+                Nogle af de semantiske tags ville være h1 til h6 overskrifter, hvor h1 ville fortælle det er, hvad siden handler om. Header eller sidehoved er også et af de semantiske tags, hvor man ofte ville have ens navigation inde i tagget og footer mere kendt på dansk som sidefod, hvor man typisk ville se ting som copyright. 
+
+                Der er også section som fortæller at det der er inde i den section høre sammen, hvilket gøre det nemmere for søgemaskiner at håndtere.  
+
+
+                Kilde til intro html https://eadania.mrooms.net/course/view.php?id=1604#section-1 pdf fra siden
+                </p>
                 </section>
                 
                 <section id="cssoverskrift">
