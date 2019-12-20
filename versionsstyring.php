41b8e724eaf86fb5d7134df020f8ad323d689bbe
GhostyGhost1337 <55685673+GhostyGhost1337@users.noreply.github.com>
2019-12-20 11:00:16 +0100

    Versionstyring

--- versionsstyring.php
+++ versionsstyring.php
@@ -13,6 +13,18 @@
              <section id="menu">
                 <?php include 'menu.inc';?>
                 </section>
-            <p>Hello world</p>
             
+            <section id="overskrift">
+            <h1>Versionsstyring</h1>
+            </section>
+            
+            <section id="version">
+            <p>Versionstyring er en metodisk tilgang og kan være så simpelt som at navngive filer og mapper. Men for multimedie designere og store firmaer ville man bruge et system til at holde styr på sine ting noget som github der kan bruges til at se ændringer fra sidst der blev gemt i en fil og man ville kunne gå tilbage, hvis der var noget der ikke virkede. Det er lidt som en tidsmaskine, hvor man kan arbejde med flere parter.</p>
+                
+            <img src="images/github.PNG">
+                
+            <p>Som set på billedet har man så en history, hvor man kan gå ind og se, hvilke ændringer der er lavet og, hvornår så med github kan man også gå tilbage til gamle versioner af sit arbejde.  Sammentidig kan man også se master branch tænkt som en gren af sit projekt, hvor man kan lave en ny branch og arbejde i ved siden af i stedet for at ændre projektet på sin master branch.</p>
+                
+            <p>Kilden til versionsstyring er <a href="https://eadania.mrooms.net/course/view.php?id=1604#section-9">powerpoint fra moodlerooms</a> og billedet er fra github desktopappen.</p>
+            </section>
         </body>
