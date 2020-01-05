aaf31ae2a36d9ec9343f9441cbeefad74b7c0cfe
GhostyGhost1337 <55685673+GhostyGhost1337@users.noreply.github.com>
2020-01-05 14:08:22 +0100

    Sidste side
    Mere styling?

--- ophavsretoglicens.php
+++ ophavsretoglicens.php
@@ -21,7 +21,106 @@
             <section id="opgave">
             <h2>Som opgave skulle vi skrive om emnet og researche</h2>
                 
-            <p></p>
+            <p>Først undersøgte jeg persondataloven</p>
+                
+            <p>Persondataloven omhandler følsomme data om individer som ikke må offentliggøres i nogen sammenhæng med mindre der er givet samtykke. Der er også håndtering af almindelige persondata som ikke kræver samtykke det kan være i forbindelse med jobs. 
+            </p>
+                
+            <p>
+            De almindelige data kunne være navn, adresse, tlf.nr, fødselsdato, familieforhold, cv, løn, arbejdstider, fravær og sygedage (men ikke årsager), kontonummer og skat plus gæld. Det ville være data som kunne håndteres i ansættelser. 
+            </p>
+            
+            <p>
+            Data som helbredsforhold, fagforening, Race eller etnisk baggrund, Politisk, religiøs eller filosofisk overbevisning, seksuelle forhold ville kræve samtykke at bruge. Det vil sige at en side som Facebook ikke kunne kræve at du oplyser dit helbred eller politisk syn for at oprette en profil da det ville være imod persondataloven. Her ville CPR numre også komme ind da de heller ikke må behandles medmindre brugeren giver samtykke til det. 
+            </p>
+
+            <p>
+            GDPR er regler for, hvordan virksomheder skal håndtere persondata ifølge deres egen hjemmeside, er der 5 vigtige regler. En virksomhed skal føre en fortegnelse som er en kortlægning af virksomhedens processer når der behandles personoplysninger.
+            </p>
+                
+            <p>
+            En virksomhed skal også dokumentere lovgivningens principper for god behandling bliver opfyldt.
+            </p>
+                
+            <p>
+            Virksomheder skal også dokumentere at de har indført passende tekniske og organisatoriske foranstaltninger.
+            </p>
+                
+            <p>    
+            Der skal oplyses til kunder og ansatte om, hvordan deres data bliver behandlet og en virksomhed skal bevise at de følger lovgivningen når det handler om samtykke. 
+            </p>
+                
+            <h2>Næst undersøgte jeg immaterielle rettigheder</h2>
+                
+            <p>Immaterielle rettigheder omhandler regler og love når det kommer til eksempelvis kunst, billeder, litteratur, opfindelser, design og forretningskendetegn.</p>
+                
+            <p>Designbeskyttelse kræver at ens design er nyt og ikke findes andre steder for at man kan få beskyttelse af loven.</p>
+                
+            <p>Domæner er også en Immateriel rettighed, hvor det handler om at den der har størst interesse i et domæne skal have det. For at overtage domæner skal man altså spørge ejeren om gratis overdragelse, betale eller klage, hvis man mener man selv har større interesse.</p>
+                
+            <p>Immaterielle rettigheder er beskyttet af ophavsretten som siger man ikke må kopiere eller stjæle uden tilladelse fra dem der har ophavsret.</p>
+                
+            <p>Har man opfundet en ny teknologi kan man søge om patent og få beskyttet sin opfindelse gennem det.</p>
+                
+            <h2>Den tredje ting jeg undersøgte var ophavsret</h2>
+                
+            <p>Ophavsret eller copyright handler om beskyttelse af værker så de ikke bliver kopieret eller brugt ulovligt.</p>
+                
+            <p>Ophavsretten beskytter immaterielle produkter af en lang række det kunne også være softwareprogrammer.</p>
+                
+            <p>Ophavsretten giver en person eller firma eneret til et produkt og giver dem lov til at fremvise og gøre det tilgængeligt for offentligheden. Det er dog tilladt at citere værket eller kopiere værker til privat forbrug men det må ikke ændres på eller bruges i en sammenhæng, hvor det krænker ophavsretsholderen.</p>
+                
+            <p>Som multimediedesigner kunne man søge om copyright på et grafisk produkt vi laver eller en hjemmeside.</p>
+                
+            <p>Når det kommer til sociale medier, må man ikke bruge andres uploads af eksempelvis billeder på deres egen hjemmeside dog skal en bruger finde sig i at ens billede kan blive delt på det sociale medie via dets funktionaliteter. Vil man linke til en youtube video på sin hjemmeside kan man også det, men brugeren der uploader skal ikke finde sig i at sit materiale bliver brugt på en krænkende måde såsom i reklamer eller politik sker dette udgør det nemlig en krænkelse af ophavsrettigheder. Vil man bruge materialer fra sociale medier til sådanne ting skal man søge samtykke fra egeren.</p>
+                
+            <h2>Den næste ting jeg undersøgte var varemærke</h2>
+                
+            <p>Et varemærke er noget der gør at en bruger kan genkende en virksomhed eksempelvis facebooks logo. En multimediedesigner kan eksempelvis ikke bruge Facbooks logo til promovere en ny hjemmeside jeg har lavet, så skulle man søge tilladelse hos Facebook om at bruge det. </p>
+                
+            <p>Kan ophavsretten beskytte en ide?: Ja kort sagt så kan den men man skal i et eksempel som at pitche en ide til et tv program skal man sørge for at man sender en detaljeret og dateret beskrivelse af sin ide på sit tv program til producenten. Så ens Ide skal være gennemført og det skal bevises at eksempelvis Tv2 har stjålet din Ide ved at et dokument kan åbnes på en senere dato som bevis for at du havde skrevet ideen ned og sendt den. </p>
+                
+            <p>Skal man registrere ophavsret af noget? Nej det sker når værket skabes men vil man have sit værk beskyttet i eksempelvis USA så er det en god ide at registrere ophavsret på sin Ide eller produkt.</p>
+                
+            <p>Er det tilladt at udnytte sig af domæne warehousing? Nej det er ikke lovligt at registrere et domænenavn med formålet at sælge det igen for at få profit.</p>
+                
+            <p>Open source betyder noget ar frit tilgængeligt eksempelvis med software kan det være kildekoden der er tilgængelig og at man derfor har lov til at redigere i det og tilpasse det til egne behov men ikke til deling eller kommercielt forbrug.</p>
+                
+            <h2>Det næste jeg undersøgte var licensaftaler</h2>
+                
+            <p>Licensaftaler er en aftale mellem 2 parter en licensgiver og en licenstager det vil mene at en licens tager ville betale for rettigheden til at bruge licensgiverens produkt eksempelvis som Dania akademiet betaler for LinkedIn sådan de studerende kan logge ind og bruge siden.</p>
+                
+            <p>jQuery er udgivet under MIT licensen som har 3 restriktioner den første er at man ikke må holde forfatteren ansvarlig de 2 andre er at man skal beholde copyright intakt og at licens navnet skal være synligt i brug af det.</p>
+                
+            <h2>Creative Commons var det sidste jeg undersøgte i opgaven</h2>
+                
+            <p>Creative Commons er en international non-profit organisation som har 6 licenser som er åbne og gratis som alle kan tage brug af.</p>
+                
+            <img src="images/ophavsret1.PNG">
+                
+            <p>De 6 licenser som de har til rådighed.</p>
+                
+            <p>De 6 har nogle ting de alle følger. Når man bruger en af de 6 har det 4 punkter de alle der brug af. 1 der skal gives credit når andre bruger ens produkt på den måde som man oplyser om. Dog må man ikke kræve at de skal tale positivt om sit værk.</p>
+                
+            <p>2 Man giver rettighed til at andre kan give dit produkt videre, vise det eller ændre i det så længe man overholder samme regler. Vil man udgive ens værk med andre regler skal man søge tilladelse.</p>
+                
+            <p>3 Alle må kopiere og bruge ens værker til alt ud over kommercielt brug medmindre de for tilladelse.</p>
+                
+            <p>4 Man skal søge tilladelse om at ændre produkter ellers må man kun vise det originale produkt.</p>
+                
+            <p>CC BY er brugt ved Freesouls https://en.wikipedia.org/wiki/Freesouls</p>
+                
+            <p>CC BY-SA er brugt ved Meat atlas https://en.wikipedia.org/wiki/Meat_Atlas</p>
+                
+            <p>CC BY-ND er brugt ved The art of unix programming https://en.wikipedia.org/wiki/The_Art_of_Unix_Programming</p>
+                
+            <p>CC BY-NC er brugt ved https://en.wikipedia.org/wiki/The_Future_of_Ideas</p>
+                
+            <p>CC BY-NC-SA er brugt ved https://en.wikipedia.org/wiki/The_Honour_of_the_Knights</p>
+                
+            <p>CC BY-NC-ND er brugt ved https://en.wikipedia.org/wiki/Move_Under_Ground</p>
+                
+            <h2>Alle kilderne brugt er sat som fodnoter i det afleverede dokument på <a href="https://eadania.mrooms.net/course/view.php?id=1604#section-22">moodlerooms</a></h2>
             </section>
             
         </body>
